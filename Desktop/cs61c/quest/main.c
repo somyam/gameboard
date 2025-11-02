eb72c0015ecad4d17e0af4211310c156b8562307
somyam <somya.mohindra@gmail.com>
2025-11-02 02:31:14 -0500

    Delete Desktop directory

--- Desktop/cs61c/quest/main.c
+++
@@ -1,8 +0,0 @@
-#include <inttypes.h>
-
-extern uint64_t bit_manip(uint64_t);
-
-int main(int argc, char *argv[]) {
-    bit_manip(0b1111111111111111111111111111111111111111111111111111111111111111)
-    return 0;
-}
